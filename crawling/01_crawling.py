51d7f71bcd0182e08e2c03ae38dcc5bf69a23258
DongCheol Jwa <vyvydkf628@naver.com>
2019-04-29 13:56:08 +0000

    fixed crawling

--- crawling/01_crawling.py
+++ crawling/01_crawling.py
@@ -14,7 +14,7 @@ soup = bs(response,"html.parser")
 # print(type(soup))
 
 #3 bs 객체에 담긴 변수에서 css selector를 활용해 특정 정보를 가져온다
-keywords = soup.select('#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div')
+keywords = soup.select('#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div span[class*=ah_k]')
 
 for index, keyword in enumerate(keywords): # 키값 벨류값
     print(index,keyword.text)
