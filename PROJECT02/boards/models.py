953c73736631423ba80b11d3e5ced1abe20e4d5a
DongCheol Jwa <vyvydkf628@naver.com>
2019-05-13 13:00:17 +0000

    updated models.py

--- PROJECT02/boards/models.py
+++ PROJECT02/boards/models.py
@@ -4,6 +4,7 @@ class Board(models.Model):
     title= models.CharField(max_length=20)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f'{self.id}번글 - {self.title}: {self.content}'
